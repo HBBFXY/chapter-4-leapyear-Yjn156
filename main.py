69a3f21a27ae3f439cd790c41c37de496d4710f9
Yjn156 <yang14932860@qq.com>
2025-09-27 14:19:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# 在此处编写代码
+def is_leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    return False
+
+try:
+    year = int(input("请输入年份: "))
+    print(is_leap_year(year))
+except ValueError:
+    print("输入无效，年份必须是整数。")
